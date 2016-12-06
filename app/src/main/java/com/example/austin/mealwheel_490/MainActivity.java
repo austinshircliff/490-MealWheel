219a8f5721fe3e6133c2c41cb20b12d027b8c76c
Austin Shircliff <shircliffa@yahoo.com>
2016-12-06 16:13:12 -0500

    Facebook and Firebase Int/Auth
    Not finished yet but getting close with sign-in

--- app/src/main/java/com/example/austin/mealwheel_490/MainActivity.java
+++ app/src/main/java/com/example/austin/mealwheel_490/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.austin.mealwheel_490;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.facebook_login);
+
+//      if(AccessToken.getCurrentAccessToken() == null) {
+//            goToLogin();
+//        }
     }
+
+
+    private void goToLogin() {
+
+        Intent intent = new Intent(this, FacebookLogin.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+
+    }
+
 }
